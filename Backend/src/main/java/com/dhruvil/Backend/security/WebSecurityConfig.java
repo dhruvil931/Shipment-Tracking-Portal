2c8a85bfc7cae832d21c44b0f063397128136bcc
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-04 13:36:48 +0530

    Solve bug

--- Backend/src/main/java/com/dhruvil/Backend/security/WebSecurityConfig.java
+++ Backend/src/main/java/com/dhruvil/Backend/security/WebSecurityConfig.java
@@ -51,7 +51,10 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://shipment-tracking-portal.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
